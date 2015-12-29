01f70a7444160f5eb6621b8307529c9ae6e0c359
Nhan Tran <nhantrantnt@gmail.com>
2015-12-28 22:11:42 -0700

    add package and stringutil

---
+++ package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/megatran/GolangSelfLearning/package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
+	fmt.Println(stringutil.MyName)
+}
