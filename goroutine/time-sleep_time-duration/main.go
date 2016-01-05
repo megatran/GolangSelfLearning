69149aca0678d8f520b15a8af561b600aef70d80
Nhan Tran <nhantrantnt@gmail.com>
2016-01-04 19:09:33 -0700

    play with go concurrency

---
+++ goroutine/time-sleep_time-duration/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	wg.Add(2)
+	go foo()
+	go bar()
+	wg.Wait()
+}
+
+func foo() {
+	for i := 0; i < 45; i++ {
+		fmt.Println("Foo:", i)
+		time.Sleep(time.Duration(3 * time.Millisecond))
+	}
+	wg.Done()
+}
+
+func bar() {
+	for i := 0; i < 45; i++ {
+		fmt.Println("Bar:", i)
+		time.Sleep(time.Duration(20 * time.Millisecond))
+	}
+	wg.Done()
+}
+
+/*
+concurrency vs. parallelism
+concurrency: doing manythings, but only one at a time-> multitasking
+parallelism: doing manything at the same time
+*/
