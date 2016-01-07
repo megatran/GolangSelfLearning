879bb22d6148f06b83173e4c9ce607083d4f38a6
Nhan Tran <nhantrantnt@gmail.com>
2016-01-07 01:11:01 -0700

    testing multiple funcs pulling values from a channel

---
+++ goroutine/channel/03_1-to-n/01_1_to_2-times/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	c := make(chan int)
+	done := make(chan bool)
+
+	go func() {
+		for i := 0; i < 100000; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	//these two funcs are pulling values from the channel
+	go func() {
+		for n := range c {
+			fmt.Println(n)
+		}
+		done <- true
+	}()
+
+	go func() {
+		for n := range c {
+			fmt.Println(n)
+		}
+		done <- true
+	}()
+
+	<-done
+	<-done
+
+}
